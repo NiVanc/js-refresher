ce4d667ffe66838eebfa291045d3e6c8572858ad
NiVanc <nico@izebra.be>
2020-03-25 19:59:22 +0100

    Understand arrow functions

--- play.js
+++ play.js
@@ -1,1 +1,29 @@
 // The playground
+
+// Arrow functions
+const name = "Nico";
+let age = 32;
+const hasHobbies = true;
+
+age = 33;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobby
+  );
+};
+
+console.log(summarizeUser(name, age, hasHobbies));
+
+const add = (a, b) => a + b;
+const addOne = a => a + 1;
+const addRandom = () => 1 + 4;
+
+console.log(add(4, 5));
+console.log(addOne(1));
+console.log(addRandom());
