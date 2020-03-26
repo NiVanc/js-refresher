06f9d2e9813298e9f2d9c7191132938f2122246e
NiVanc <nico@izebra.be>
2020-03-26 08:11:20 +0100

    Understand this and function references

--- play.js
+++ play.js
@@ -18,12 +18,18 @@ const summarizeUser = (userName, userAge, userHasHobby) => {
   );
 };
 
-console.log(summarizeUser(name, age, hasHobbies));
+// console.log(summarizeUser(name, age, hasHobbies));
 
 const add = (a, b) => a + b;
 const addOne = a => a + 1;
 const addRandom = () => 1 + 4;
 
-console.log(add(4, 5));
-console.log(addOne(1));
-console.log(addRandom());
+// console.log(add(4, 5));
+// console.log(addOne(1));
+// console.log(addRandom());
+
+// this and function references
+// https://www.youtube.com/watch?v=Pv9flm-80vM&feature=emb_logo
+// This refers to what is executing the code
+// Use bind to tell JS what this should be referring to in the function that will be executed
+// When using arrow functions, this always refers to the object
